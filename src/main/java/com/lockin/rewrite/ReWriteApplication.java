c5669f75e15e42c4a4baff0164c47929d3275c00
Aswath-Senthilkumar <asenth25@asu.edu>
2026-03-02 13:48:26 -0700

    Initial commit: Spring Boot backend boilerplate

---
+++ src/main/java/com/lockin/rewrite/ReWriteApplication.java
@@ -0,0 +1,13 @@
+package com.lockin.rewrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReWriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lockin/rewrite/ReWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockin.rewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
