488b9313f2543412c2ca8e1d5592b48804a0e047
Aswath-Senthilkumar <asenth25@asu.edu>
2026-03-03 19:07:43 -0700

    Implemented Pdf and Word parser service

--- src/main/java/com/lockin/rewrite/service/DocumentParserService.java
+++ src/main/java/com/lockin/rewrite/service/DocumentParserService.java
@@ -1,4 +1,34 @@
 package com.lockin.rewrite.service;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@Service
 public class DocumentParserService {
+
+    public String parseDocx(byte[] docxBytes) {
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(docxBytes);
+                XWPFDocument document = new XWPFDocument(inputStream);
+                XWPFWordExtractor extractor = new XWPFWordExtractor(document)) {
+            return extractor.getText();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse DOCX", e);
+        }
+    }
+
+    public String parsePdf(byte[] pdfBytes) {
+        try (PDDocument document = Loader.loadPDF(pdfBytes)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(document);
+        } catch (IOException e) {
+            throw  new RuntimeException("Failed to parse PDF", e);
+        }
+    }
 }
